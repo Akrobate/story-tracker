ba30e116a8a52d30a56a86216a54614e2f8618ca
fedorov.artiom <fedorov.artiom@635b767d-3f64-b619-a1db-c37717af5f48>
2014-10-08 22:38:27 +0000

    Ajout principal

---
+++ templates/statuses/add.php
@@ -0,0 +1,24 @@
+<!-- template ajouter page -->
+
+<div class="row">
+
+
+<div class="col-md-4 col-md-offset-4">
+
+	<button type="button" class="btn btn-default btn-lg btn-block" data-status="10">dév</button>
+	<button type="button" class="btn btn-primary btn-lg btn-block" data-status="10">tests tech</button>
+	<button type="button" class="btn btn-default btn-lg btn-block" data-status="10">tests</button>
+	<button type="button" class="btn btn-default btn-lg btn-block" data-status="10">retours recette</button>
+	<button type="button" class="btn btn-default btn-lg btn-block active" data-status="10">Terminé</button>
+	<button type="button" class="btn btn-default btn-lg btn-block" data-status="10">Stand By</button>
+
+
+<br /> <br/>
+
+		<button type="button" class="btn btn-success btn-lg btn-block" data-status="10">Valider</button>
+
+		<button type="button" class="btn btn-danger btn-lg btn-block" data-status="10">Annuler</button>
+
+	</p>
+</div>
+</div>
