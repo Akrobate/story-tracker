efd66e9d70bbd237c9de37ad9d624fa142e55c62
fedorov.artiom <fedorov.artiom@635b767d-3f64-b619-a1db-c37717af5f48>
2014-10-10 00:54:39 +0000

    Commit en cours

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+	session_start();
+	error_reporting(15);
+	require_once("./api.php");
+	
+	$controller = request::get("controller");
+	$action = request::get("action");
+	
+	if ($controller == "") {
+		$controller = "stories";
+	}
+	
+	if($action == "") {
+		$action = "index";
+	}
+	
+	$action_tab = explode('-', $action);
+	if ($action_tab[0] == 'ajax') {
+		if(file_exists(PATH_SCRIPTS . $controller . "/" . $action . ".php")) {
+			require_once (PATH_SCRIPTS . $controller . "/" . $action . ".php");
+		}
+
+	} else {
+		if ((file_exists(PATH_SCRIPTS . $controller . "/" . $action . ".php")) 
+			&& ((PATH_TEMPLATES . $controller . "/" . $action . ".php"))) {
+				require_once (PATH_SCRIPTS . $controller . "/" . $action . ".php");
+				ob_start();
+					require_once (PATH_TEMPLATES . $controller . "/" . $action . ".php");
+				//$template_content = ob_get_contents();
+				$template_content = ob_get_contents();
+				ob_end_clean();
+				require_once( PATH_TEMPLATES . "layouts/mainBootstrap3.php");	
+		} else {
+			echo("ERROR::Template or script missing");
+		}
+	}
